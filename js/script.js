98f407b7a6d1ac03a90cc08694fe2a188e52c738
dmtiriyy <dim.evtushenckoyandex.ua@gmail.com>
2022-10-31 22:37:37 +0200

    work with GET request

--- js/script.js
+++ js/script.js
@@ -214,8 +214,23 @@ class MenuCard {
 
 }); 
 
-const menu = {
-    name:'s'
-}
-console.log (JSON.stringify(menu));
-JSON.parse.(JSON.stringify(screen));
+const inputUah = document.querySelector('#uah'),
+    inputUsd = document.querySelector('#usd');
+
+  inputUah.addEventListener('input', () => {
+    const request = new XMLHttpRequest();
+
+    request.open('GET','js/current.json');
+    request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+    request.send();
+
+    request.addEventListener('load', () => {
+        if( request.status ===200){
+            const data = JSON.parse(request.response);
+            inputUsd.value = (+inputUah.value / data.current.usd).toFixed(2); 
+        } else {
+            inputUsd.value = 'Something wrong'
+        }
+    });
+    
+  }); 
