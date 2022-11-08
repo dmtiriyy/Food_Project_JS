52df099086a012156e809cf40f5c891614796ecd
dmtiriyy <dim.evtushenckoyandex.ua@gmail.com>
2022-11-08 22:06:10 +0200

    add methods

--- js/script.js
+++ js/script.js
@@ -286,3 +286,20 @@ getResource('http://localhost:3000/menu')
             .then(res => console.log(res));
      
 }); 
+const funds = [
+    {amount: -1400},
+    {amount: 2400},
+    {amount: -1000},
+    {amount: 500},
+    {amount: 10400},
+    {amount: -11400}
+];
+
+const getPositiveIncomeAmount = (data) => {
+    return data.filter(item => item.amount > 0).reduce((sum, curr) => sum+ curr.amount, 0)
+};
+    console.log(getPositiveIncomeAmount(funds))
+const getTotalIncomeAmount = (data) => {
+    return data.some(item => item.amount <=0) ? data.reduce((sum, curr) => sum+ curr.amount, 0) : getPositiveIncomeAmount(data) 
+};
+console.log(getTotalIncomeAmount(funds));
