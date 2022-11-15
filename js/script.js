859d1a158d17157dff353d4cd9d3ccdfae9df4cc
dmtiriyy <dim.evtushenckoyandex.ua@gmail.com>
2022-11-15 16:21:28 +0200

    add localStorage

--- js/script.js
+++ js/script.js
@@ -543,5 +543,41 @@ getResource('http://localhost:3000/menu')
     getDynamicInformation('#height');
     getDynamicInformation('#weight');
     getDynamicInformation('#age');
+    const checkbox = document.querySelector('#checkbox'),
+    form = document.querySelector('form'),
+    change = document.querySelector('#color');
+
+    if(localStorage.getItem('isChecked')){
+        checkbox.checked = true;
+    } 
+
+
+    if(localStorage.getItem('bg') === 'changed') {
+        form.style.backgroundColor = 'red'
+    }   
+checkbox.addEventListener('change', () =>{
+    localStorage.setItem('isChecked', true);
+})
+
+
+change.addEventListener('click', () =>{
+    if(localStorage.getItem('bg') === 'changed') {
+        localStorage.removeItem('bg');
+        form.style.backgroundColor = '#fff'
+    }else{
+        localStorage.setItem('bg', 'changed');
+        form.style.backgroundColor = 'red';
+    }
+});
+
+const persone = {
+    name: 'Alex',
+    age: 25
+};
+
+const serializedPersone = JSON.stringify(persone);
+localStorage.setItem('alex', serializedPersone);
+
+console.log(JSON.parse(localStorage.getItem('alex')));
      });
  
