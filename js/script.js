9aed6b83449de5c4e5b383b7f7d4c9cdba24873b
dmtiriyy <dim.evtushenckoyandex.ua@gmail.com>
2022-11-16 21:41:10 +0200

    try get set

--- js/script.js
+++ js/script.js
@@ -543,41 +543,19 @@ getResource('http://localhost:3000/menu')
     getDynamicInformation('#height');
     getDynamicInformation('#weight');
     getDynamicInformation('#age');
-    const checkbox = document.querySelector('#checkbox'),
-    form = document.querySelector('form'),
-    change = document.querySelector('#color');
-
-    if(localStorage.getItem('isChecked')){
-        checkbox.checked = true;
-    } 
-
-
-    if(localStorage.getItem('bg') === 'changed') {
-        form.style.backgroundColor = 'red'
-    }   
-checkbox.addEventListener('change', () =>{
-    localStorage.setItem('isChecked', true);
-})
-
-
-change.addEventListener('click', () =>{
-    if(localStorage.getItem('bg') === 'changed') {
-        localStorage.removeItem('bg');
-        form.style.backgroundColor = '#fff'
-    }else{
-        localStorage.setItem('bg', 'changed');
-        form.style.backgroundColor = 'red';
-    }
-});
-
-const persone = {
-    name: 'Alex',
-    age: 25
-};
-
-const serializedPersone = JSON.stringify(persone);
-localStorage.setItem('alex', serializedPersone);
-
-console.log(JSON.parse(localStorage.getItem('alex')));
+    const persone = {
+        name: 'Alex',
+        age: 25,
+    
+        get userAge() {
+            return this.age;
+        },
+    
+        set userAge(num) {
+            this.age = num;
+        }
+    };
+    console.log(persone.userAge = 30);
+    console.log(persone.userAge);
      });
  
