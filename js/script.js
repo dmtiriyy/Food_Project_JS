90c0e790c5d1717bdc886bab0d3b65978f64255f
dmtiriyy <dim.evtushenckoyandex.ua@gmail.com>
2022-12-02 18:17:24 +0200

    add new constructors

--- js/script.js
+++ js/script.js
@@ -552,5 +552,14 @@ getResource('http://localhost:3000/menu')
         User.prototype.exit = function() {
             console.log(`Пользователь ${this.name} ушел`)
         };
+        const ivan = new User('Ivan', 28);
+        const alex = new User('Alex', 22);
+        ivan.exit();
+
+        ivan.hello();
+        alex.hello();
+
+        console.log(ivan);
+        console.log(alex);
      });
  
